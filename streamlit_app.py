5bb6d1b64de840b59fa39efa0a6c6645e21bb538
Shiva Charan Reddy Kallem <110608023+ShivaCharanReddyK@users.noreply.github.com>
2025-12-28 18:43:55 +0530

    Change image display to use column width

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if uploaded_file is not None:
     
     with col1:
         image = Image.open(uploaded_file).convert("RGB")
-        st.image(image, caption="Uploaded Image", use_container_width=True)
+        st.image(image, caption="Uploaded Image", use_column_width=True)
     
     # Make prediction
     with st.spinner('Analyzing image...'):
